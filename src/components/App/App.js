3461926cb552d7f1dfeb28213d31a9394baa6a83
Mark Murray <mmmarkmurray@gmail.com>
2016-09-16 10:33:35 -0700

    remove Header comp, App comp stateless

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,36 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { dependencies } from 'package.json';
 
-// components
-import Header from 'Header/Header';
+export default function App() {
+  return (
+    <div class="app">
+      <h1 style={{ textAlign: 'center' }}>New React Project</h1>
+      <div class="container">
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-  }
+        <h1>Features!</h1>
+        <ul>
+          <li><a href="https://facebook.github.io/react/">React ({dependencies['react']})</a></li>
+          <li><a href="https://github.com/reactjs/react-router">React Router ({dependencies['react-router']})</a></li>
+          <li><a href="https://babeljs.io/">Babel</a></li>
+          <li><a href="https://github.com/gaearon/react-hot-loader">React Hot Loader</a></li>
+          <li><a href="https://webpack.github.io/docs/webpack-dev-server.html">Webpack Dev Server</a></li>
+          <li><a href="https://github.com/jtangelder/sass-loader">Sass</a> with <a href="https://github.com/passy/autoprefixer-loader">Autoprefixer</a></li>
+        </ul>
 
-  render() {
+        <h1>Generate Components</h1>
 
-    return (
-      <div class="app">
-        <Header />
-        <div class="container">
-
-          <h1>Features!</h1>
-          <ul>
-            <li><a href="https://facebook.github.io/react/">React ({dependencies['react']})</a></li>
-            <li><a href="https://github.com/reactjs/react-router">React Router ({dependencies['react-router']})</a></li>
-            <li><a href="https://babeljs.io/">Babel</a></li>
-            <li><a href="https://github.com/gaearon/react-hot-loader">React Hot Loader</a></li>
-            <li><a href="https://webpack.github.io/docs/webpack-dev-server.html">Webpack Dev Server</a></li>
-            <li><a href="https://github.com/jtangelder/sass-loader">Sass</a> with <a href="https://github.com/passy/autoprefixer-loader">Autoprefixer</a></li>
-          </ul>
-
-          <h1>ProTip</h1>
-
-          <p>Install <a href="https://github.com/markmur/react-component-gen">react-component-gen</a> to start generating components from the command line.</p>
-        </div>
+        <p>Install <a href="https://github.com/markmur/react-component-gen">react-component-gen</a> to start generating components from the command line.</p>
       </div>
-    );
-  }
+    </div>
+  );
 }
