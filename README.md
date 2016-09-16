fa1ef30bd60a77644ae927a149cdf19f30ce6e68
Mark Murray <mmmarkmurray@gmail.com>
2016-09-15 23:56:20 -0700

    Base

---
+++ README.md
@@ -0,0 +1,38 @@
+# New React Project
+
+A scaffolding tool for generating simple [React](https://facebook.github.io/react/) apps with some default.
+
+## Includes
+
+* [Sails](http://sailsjs.org) v0.12.2
+* [React](https://facebook.github.io/react/) v15.0.1
+* [Babel with ES2015](https://babeljs.io)
+* [React Router](https://github.com/reactjs/react-router)
+* [Sass](https://github.com/jtangelder/sass-loader)
+* [Autoprefixer](https://github.com/passy/autoprefixer-loader)
+* [React Hot Loader](https://github.com/gaearon/react-hot-loader)
+
+___
+
+### Clone
+
+```shell
+git clone https://github.com/markmur/new-react-project.git
+cd new-react-project
+npm install
+```
+
+___
+
+### Run (development)
+
+`npm start`
+
+To view your app, go to `http://localhost:3000` in your browser.
+___
+
+### Run (Production)
+
+`npm run build`
+
+___
---
+++ webpack.config.dev.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const webpack = require('webpack');
+const NODE_ENV = process.env.NODE_ENV;
+
+module.exports = {
+  target: 'web',
+
+  devtool: 'cheap-module-eval-source-map',
+
+  entry: [
+    'babel-polyfill',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/dev-server',
+    './src/main.js',
+  ],
+
+  output: {
+    path: path.join(__dirname, '/public/dist/'),
+    filename: 'bundle.js',
+    pathInfo: true,
+    publicPath: 'http://localhost:8080/dist/',
+    hot: true,
+  },
+
+  resolve: {
+    root: path.join(__dirname, ''),
+    modulesDirectories: [
+      'node_modules',
+      'src',
+      'src/components',
+      'src/styles',
+    ],
+    extensions: ['', '.js', '.jsx'],
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      __ENV__: NODE_ENV,
+    }),
+  ],
+
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/, // sass files
+        loader: 'style!css!autoprefixer?browsers=last 2 version!sass?outputStyle=expanded',
+      },
+      {
+        test: /\.(ttf|eot|svg|woff)(\?[a-z0-9]+)?$/, // font files
+        loader: 'file-loader?name=[path][name].[ext]',
+      },
+      {
+        test: /\.(js|jsx)?$/, // react files
+        exclude: /node_modules/,
+        loaders: ['react-hot', 'babel?' + JSON.stringify({
+          presets: ['es2015', 'stage-0', 'react'],
+          plugins: ['transform-decorators-legacy']
+        })],
+      }
+    ],
+
+    noParse: /\.min\.js/,
+  },
+};
